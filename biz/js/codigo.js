b500075a4aaf70dc90cbdab1e780f078b4010bd6
Enrique Cardoza Quispe <anewryzm@gmail.com>
2013-02-23 01:56:12 -0500

    loading images

--- biz/js/codigo.js
+++ biz/js/codigo.js
@@ -3,6 +3,5 @@ $(document).ready(function(){
 		$(this).fadeIn("slow")
 	})
 	$(".blank img, #player").hide();
-	// $(".blank img").delay(500).fadeIn("slow");
 	$("#player").delay(800).fadeIn("slow");
 });
