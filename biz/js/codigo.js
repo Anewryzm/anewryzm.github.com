89fb3bce002dbb7c7cae2b97b5a77d19ca89cc35
Enrique Cardoza Quispe <anewryzm@gmail.com>
2013-02-23 01:51:48 -0500

    load test

--- biz/js/codigo.js
+++ biz/js/codigo.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
+	$("img").load(function(){
+		$(this).fadeIn("slow")
+	})
 	$(".blank img, #player").hide();
-	$(".blank img").delay(500).fadeIn("slow");
+	// $(".blank img").delay(500).fadeIn("slow");
 	$("#player").delay(800).fadeIn("slow");
 });
